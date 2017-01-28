eb48e3b4c86ff80d2a1e2488d8a0ba9b00a42b20
Ravenwater <theo@stillwater-sc.com>
2017-01-28 18:51:27 -0500

    Adding Domain of Computation struct

---
+++ indexspace/domainofcomputation.go
@@ -0,0 +1,27 @@
+/*
+ File		:	$File: //depot/stillwater-sc/godfa/indexspace/domainofcomputation.go $
+
+ Authors	:	E. Theodore L. Omtzigt
+ Date		:	6 April 2014
+
+ Source Control Information:
+ Version	:	$Revision: #1 $
+ Latest		:	$Date: 2014/04/06 $
+ Location	:	$Id: //depot/stillwater-sc/godfa/indexspace/domainofcomputation.go#1 $
+
+ Organization:
+		Stillwater Supercomputing, Inc.
+		P.O Box 720
+		South Freeport, ME 04078-0720
+
+Copyright (c) 2006-2017 E. Theodore L. Omtzigt.  All rights reserved.
+
+Licence      : Stillwater license as defined in this directory
+
+ */
+package indexspace
+
+type DomainOfComputation struct {
+	A ConstraintSet
+	b Constraint
+}
