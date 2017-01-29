d70c8e6e66c2adcc0dd64f288873da4e1bc7680d
Ravenwater <theo@stillwater-sc.com>
2017-01-29 08:00:39 -0500

    Adding tests for 100% coverage

--- indexspace/constraintset_test.go
+++ indexspace/constraintset_test.go
@@ -26,17 +26,15 @@ import (
 	"fmt"
 )
 
-func TestNewConstraintSet(t *testing.T) {
+func TestConstraintSet(t *testing.T) {
 	c1 := NewConstraint(3)
 	c1[0] = 1
 	cs := NewConstraintSet()
-	cs, err := cs.AddConstraint(c1)
-	if err != nil {
-		t.Error(err)
-	}
+	cs, _ = cs.AddConstraint(c1)	// on the first it can't really throw any errors
+
 	c2 := NewConstraint(3)
 	c2[1] = 1
-	cs, err = cs.AddConstraint(c2)
+	cs, err := cs.AddConstraint(c2)	// on subsequent calls you may submit a constraint with a different dimensionality
 	if err != nil {
 		t.Error(err)
 	}
@@ -54,5 +52,64 @@ func TestNewConstraintSet(t *testing.T) {
 	if cc[0] != c1[0] {
 		t.Error("Constraint didn't persist properly")
 	}
+
+}
+
+func TestConstraintSet_SpecialConditions(t *testing.T) {
+	c1 := Constraint{1,0,0}
+	cs := NewConstraintSet()
+	cs, _ = cs.AddConstraint(c1)
+	c2 := Constraint{1,0,0,0}
+	cs, err := cs.AddConstraint(c2)
+	if err == nil {
+		t.Error("ConstraintSet.AddConstraint didn't recognized incompatible constraint dimensions")
+	}
+	c2, err = cs.GetConstraint(2)
+	if err == nil {
+		t.Error("ConstraintSet.GetConstraint didn't recognize out of bounds index")
+	}
+}
+
+func TestConstraintSet_String(t *testing.T) {
+	c1 := Constraint{1,0,0,-1,0,0}
+	c1_str := c1.String()
+	c1_ref := "  1   0   0  -1   0   0 "   // notice the trailing space
+	if c1_str != c1_ref {
+		t.Error("Constraint stringer failed")
+	}
+	c2 := Constraint{0,1,0,0,-1,0}
+	c3 := Constraint{0,0,1,0,0,-1}
+	cs := NewConstraintSet()
+	cs_str := cs.String()		// empty constraint set
+	cs_ref := "Empty Constraint Set"
+	if cs_str != cs_ref {
+		t.Error("ConstraintSet stringer didn't recognize an empty set")
+	}
+	cs, _ = cs.AddConstraint(c1)
+	cs, _ = cs.AddConstraint(c2)
+	cs, _ = cs.AddConstraint(c3)
+	cs_str = cs.String()
+	cs_ref = "  1   0   0 \n  0   1   0 \n  0   0   1 \n -1   0   0 \n  0  -1   0 \n  0   0  -1 \n"
+	if cs_str != cs_ref {
+		t.Error("ConstraintSet stringer failed")
+		fmt.Printf("%x\n", cs_str)
+		fmt.Printf("%x\n", cs_ref)
+	}
+}
+
+func ExampleConstraintSet() {
+	c1 := NewConstraint(3)
+	c1[0] = 1
+	cs := NewConstraintSet()
+	cs, _ = cs.AddConstraint(c1)	// on the first it can't really throw any errors
+	c1 = Constraint{0,1,0}
+	cs, _ = cs.AddConstraint(c1)
+	c2 := Constraint{0,0,1}
+	cs, _ = cs.AddConstraint(c2)
 	fmt.Print(cs)
+
+	// OUTPUT
+	//   1   0   0
+	//   0   1   0
+	//   0   0   1
 }
